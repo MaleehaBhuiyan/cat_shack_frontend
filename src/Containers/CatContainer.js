ca4037e3fae7531ee59da17069ffb7dc089cff3e
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-10-29 23:15:26 -0400

    Working on CatContainer

---
+++ src/Containers/CatContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CatContainer extends React.Component{
+    render(){
+        return(
+            <>
+            Hi from cat container 
+            </>
+        )
+    }
+} 
+
+export default CatContainer
